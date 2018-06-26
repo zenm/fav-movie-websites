f6c878ca65ca9917f3bf2ba3d119574de3fb8d25
Lino <linojr@gmail.com>
2018-06-26 06:36:01 -0500

    Added to  read me, added movie instances

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Favorite Movie Trailer Project
+----
+A movie trailer project as part of the Udacity Nano Degree program.
+
+## Getting Started
+----
+These instructions will get the project up on your local machine.
+
+### Prerequisites
+```
+Python 2
+```
+### Installing
+
+In your terminal, download or clone the repo from `location/on/git`
+```
+$ git clone `locatoin\on\git`
+```
+
+
+### Built With
+* Python
+* JQuery
+* Bootstrap
+
+### Acknowledgements
+----
+* Thanks, [Udacity](https://www.udacity.com/).
---
+++ media.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+class Video():
+    '''Used to create instance of videos with a title'''
+    def __init__(self, media_title):
+        self.title = media_title
+
+class Movie(Video):
+    '''Used to create instances of Movies, shows trailers and stores info on storyline, poster and a youtube url'''
+    def __init__(self, title, storyline, poster_image, youtube_trailer):
+        Video.__init__(self, title)
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = youtube_trailer
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
